3ea600ffa894d1030693df20f2fcfe03ee6efa34
EmmanuelDeveloper-19 <kouseimitsuhashi@gmail.com>
2026-08-23 13:51:14 -0600

    Restructuración de los archivos de configuración

--- aplicacion/app/init.php
+++ aplicacion/app/init.php
@@ -5,7 +5,7 @@ require_once("core/db.php");
 require_once("core/controller.php");
 
 define("MAIN_ROOT", __DIR__);
-define("PATH", "/budgetingapplication/aplicacion/public/");
+define("PATH", "/");
 define("MODELS", __DIR__ . "/models/");
 define("CONTROLLERS", __DIR__ . "/Controllers/");
 define("VIEWS", __DIR__ . "/Views/");
